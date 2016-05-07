69f36fba708af5ef37391ff3a971c5af5d51088e
Uros Majcen <um2986@student.uni-lj.si>
2016-05-07 20:33:16 +0000

    Zaključek dela na III. nalogi

--- eracun.js
+++ eracun.js
@@ -133,7 +133,10 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      callback(vrstice);
+      for (var i=0; i<vrstice.length; i++) {
+          vrstice[i].stopnja = davcnaStopnja((vrstice[i].opisArtikla.split(' (')[1]).split(')')[0], vrstice[i].zanr);
+        }
+        callback(vrstice);
     })
 }
 
